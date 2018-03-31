4ed0c881f7cba8dc5aafac653ab503628fa68ab6
jimthecactus <jimthecactus@gmail.com>
2018-03-30 18:01:19 -0600

    Precalculated data to minimize CPU/Power Demands
    Might be worth it to seperate it into two tables.
    Would save ROM at the expense of CPU/Power.
    Could be cool if I want to get below 2048 bytes.

--- mod_mod.c
+++ mod_mod.c
@@ -1,19 +1,11 @@
 #include <avr/io.h>
-#include <math.h>
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
+#include "note_table.h"
 
 #define PRESCALER 2 // 2 selects the /8 prescaler
-#define PRESCALER_RATIO 8
-#define TICK_PERIOD (1.0/(float)(F_CPU)*(float)(PRESCALER_RATIO))
 #define A440 2273 // Ticks assuming 8MHz clock and /8 prescaler
 #define A440_BY_2 (A440>>1) // Half so we can toggle
-#define SEMITONES_PER_OCTAVE 12 // From music theory
-#define SCALE_BOTTOM (24+12) // Note number on a scale where A440 is 57
-#define SCALE_TOP (SCALE_BOTTOM+SEMITONES_PER_OCTAVE*2) // 2 octaves
-#define SCALE_RANGE (float)(SCALE_TOP-SCALE_BOTTOM)/(float)(1024)
-#define REF_FREQENCY (float)440.0 // Call A440 our reference frequency
-#define REF_NOTE 57 // Call 57 the reference note number for A440.
 #define OSC_OFF (unsigned char) ~(3<< CS10); // All 0s for the CS regs.
 
 
@@ -50,7 +42,7 @@ int main(void) {
 		1 << ADEN |  // Enable ADC
 		1 << ADATE | // Enable auto trigger
 		1 << ADIE |  // Enable interrupt
-		6 << ADPS0;  // use /64 prescaler
+		7 << ADPS0;  // use /128 prescaler
 	ADCSRB = 0 << ADTS0; // Select free running auto trigger.
 	DIDR0 = 1 << ADC0D; // Disable digital IO buffer on ADC0
 
@@ -65,39 +57,20 @@ int main(void) {
 	sei();	
 	
 	while (1) {
-		// Check if the button is pressed
-		if (PINB & 0x01) { // If not (inverse logic)
-			TCCR1B &= OSC_OFF; // Kill the oscillator
-		}
-		else { // Otherwise
-			TCCR1B |= PRESCALER; // Turn the oscillator on.
-		}
 		// Take a nap to save power till the next conversion finishes.
 		sleep_mode();
 	}
 }
 
 ISR(ADC_vect) {
-	// Initialize to the reference note.
-	static float oldnote = REF_NOTE; 
-		
-	float input = (float) ADC;  // Get the input value
-	// Calculate the new note number
-	float note = (input * SCALE_RANGE + (float)SCALE_BOTTOM);
-	// Average the new note with the old one with 1/3rd feedback
-	float note_with_feedback = round((note * 2.0 + oldnote) / 3.0);
-	// Cache the old note for hysteresis. 
-	oldnote = note_with_feedback;
-	// Get the delta relative to our reference note offset by half for tuning.
-	float note_offset = note_with_feedback - ((float)REF_NOTE - 0.5);
-	// Calculate the exact fractional octave for our note
-	float octave = note_offset / (float) SEMITONES_PER_OCTAVE;
-	// Calculate the frequency of the note given it's octave value
-	float freq = REF_FREQENCY * pow(2, octave);
-	// Get the note's period and cut it in half since we're toggling.
-	float period = (1.0 / freq) / 2.0;
-	// Get the number of ticks it's we need to wait between toggles
-	unsigned int counts = (period / TICK_PERIOD);
-	// Write the calculated 16 bit number into the register.	
-	OCR1A = counts; 
+	// Check if the button is pressed
+	if (PINB & 0x01) { // If not (inverse logic)
+		TCCR1B &= OSC_OFF; // Kill the oscillator
+	}
+	else { // Otherwise
+		unsigned int input = ADC; // Get the input
+		unsigned int period = get_period(input); // Lookup the oscillator period
+		OCR1A = period; // Write it to the oscillator
+		TCCR1B |= PRESCALER; // Turn the oscillator on
+	}	
 }
